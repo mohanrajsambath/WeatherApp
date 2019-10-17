1b4c7f32174196da0dfd6f1725fe902d6584d5c4
ganeshkumarraja <ganeshkumardeveloper@gmail.com>
2019-10-18 01:23:55 +0200

    1.Voice recogonizer added 2.response has been transmistted from view model to activity with live data 3. UI test has been written
      both are done by GaneshKumar on 18/10/2019 at 1:19 AM

--- app/src/main/java/com/ganesh/weatherapp/view/BaseActivity.kt
+++ app/src/main/java/com/ganesh/weatherapp/view/BaseActivity.kt
@@ -1,10 +1,15 @@
 package com.ganesh.weatherapp.view
 
+
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
+import android.net.ConnectivityManager
 
 
 abstract class BaseActivity : FragmentActivity() {
+
+    val requesCode = 11
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -14,6 +19,22 @@ abstract class BaseActivity : FragmentActivity() {
     }
 
     abstract fun initDI()
+
+    fun showMessage(message: String) {
+
+        Toast.makeText(
+            applicationContext,
+            message,
+            Toast.LENGTH_SHORT
+        ).show()
+
+    }
+
+
+    fun isNetworkConnected(): Boolean {
+        val cm = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        return cm.activeNetworkInfo != null && cm.activeNetworkInfo.isConnected
+    }
 
 
 }
