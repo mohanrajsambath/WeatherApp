58c2d99f12e58f84ebe8a997cc3659c494c48588
ganeshkumarraja <ganeshkumardeveloper@gmail.com>
2019-10-18 13:07:14 +0200

    Method name changed in ViewModel class

---
+++ app/src/androidTest/java/com/ganesh/weatherapp/WeatherActvityTest.kt
@@ -0,0 +1,47 @@
+package com.ganesh.weatherapp
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.ganesh.weatherapp.view.WeatherActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class WeatherActvityTest {
+
+
+    @get:Rule
+    var activityRule = ActivityTestRule(WeatherActivity::class.java)
+
+
+    @Test
+    fun successMessageTest() {
+
+        val responseData = UITestData().getData()
+
+        activityRule.activity.setValueToViews(responseData)
+
+        onView(withId(R.id.cityName)).check(matches(withText("")))
+
+    }
+
+
+    @Test
+    fun failureMessageTest() {
+
+        activityRule.activity.setErorrMessage("Invalid City Name")
+
+        onView(withId(R.id.errorContent)).check(matches(withText("Invalid City Name")))
+
+    }
+
+
+
+
+}
